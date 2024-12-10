ea319182c017585c5504f6096dd3278044e44a52
Dmytro <eazy777er@gmail.com>
2024-12-10 23:02:24 +0100

    I made all the tabs responsive from 390 px up and limited max width by 640px

--- README.md
+++ README.md
@@ -10,10 +10,13 @@ tillbehör buttons color: #837c7c
 
 frågor?
 1 vilken font exakt?
-2 syfte med korg i order flik?
+2 syfte med korg i order flik? ------ skapa tillbaka button
 -skulle det inte vara knappen tillbaka om man vill köpa något annat från menu?
 3 MOMS för livsmedel och mat är 12%
-4 Guacamole i order flik har ingen border-bottom?
+4 Guacamole i order flik har ingen border-bottom - bara ta bort under sista
+5 röda notiser hide om = 0
+6. hover på kanppar
+7. gör MAX width på alla flik.s kontainers
 
 
 
--- javascript/tabs_show_hide.js
+++ javascript/tabs_show_hide.js
@@ -3,13 +3,14 @@
 const menuCartButton = document.querySelector(`.menu-cartBTN`)
 const orderCartButton = document.querySelector(`.order-cartBTN `)
 const payButton = document.querySelector(`.paymentBtn`)
-const receiptButton = document.querySelector(`.eta-receipt`)
+const receiptButton = document.querySelector(`.eta-receiptBtn`)
 const newOrderButton = document.querySelector(`.eta-new-orderBtn`)
 const receiptNewOrderButton = document.querySelector(`.receipt-new-orderBtn`)
 const menuTab = document.querySelector(`.menu`)
 const orderTab = document.querySelector(`.order`)
 const wontonBoxTab = document.querySelector(`.eta`)
 const receiptTab = document.querySelector(`.receipt`)
+const bodyElement = document.querySelector(`body`)
 
 
 // ---------------show or hide TABs---------------
@@ -17,17 +18,19 @@ const receiptTab = document.querySelector(`.receipt`)
 menuCartButton.addEventListener("click", () => {
 	menuTab.classList.add(`hidden`)
 	orderTab.classList.remove(`hidden`)
+	bodyElement.style.backgroundColor = "var(--order-bg-color)"
 })
 
-//--------what's a function of the cartBTN in orderTAB?-----
-// menuCartButton.addEventListener("click", () => {
-// 	menuTab.classList.add(`hidden`)                 
-// 	orderTab.classList.remove(`hidden`)
-// })
+orderCartButton.addEventListener("click", () => {
+	menuTab.classList.remove(`hidden`)                 
+	orderTab.classList.add(`hidden`)
+	bodyElement.style.backgroundColor = "#489078"
+})
 
 payButton.addEventListener("click", () => {
 	orderTab.classList.add(`hidden`)
 	wontonBoxTab.classList.remove(`hidden`)
+	bodyElement.style.backgroundColor = "var(--gray-bg-color)"
 })
 receiptButton.addEventListener("click", () => {
 	wontonBoxTab.classList.add(`hidden`)
@@ -36,8 +39,10 @@ receiptButton.addEventListener("click", () => {
 newOrderButton.addEventListener("click", () => {
 	wontonBoxTab.classList.add(`hidden`)
 	menuTab.classList.remove(`hidden`)
+	bodyElement.style.backgroundColor = "#489078"
 })
 receiptNewOrderButton.addEventListener("click", () => {
 	receiptTab.classList.add(`hidden`)
 	menuTab.classList.remove(`hidden`)
+	bodyElement.style.backgroundColor = "#489078"
 })
